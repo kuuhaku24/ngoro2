4c129eac3209bf770bbf14531221ed83116f30cb
kuuhaku24 <yasaf.hafidz@gmail.com>
2022-07-07 18:37:30 +0700

    aksi

--- user/aksi.php
+++ user/aksi.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "mysql";
-$dbname = "nganjukportal";
+$password = "";
+$dbname = "ngoro";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
